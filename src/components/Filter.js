4602ae71fefa1d83eb45bb87d59b4a645ce882b4
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-28 16:47:08 -0700

    select menu in filterjs, handle change function and series state created

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Filter() {
-  return <div>Filter</div>;
+  const [series, setSeries] = useState('StardustCrusaders');
+
+  const handleSelect = (select) => {
+    setSeries(select);
+  };
+
+  return (
+    <div>
+      <select onChange={(e) => handleSelect(e.target.value)}>
+        <option value="StardustCrusaders">Stardust Crusaders</option>
+        <option value="DiamondIsUnbreakable">Diamond Is Unbreakable</option>
+        <option value="GoldenWind">Golden Wind</option>
+      </select>
+    </div>
+  );
 }
