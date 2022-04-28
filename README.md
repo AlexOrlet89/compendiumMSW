7b6db3a62e73b09737572385b45b75befbd8d453
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-28 16:10:22 -0700

    data is destructured, the values of our keys are arrays and those arrays are grabbed and then spreadinto one large array

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Where else to start but at the beginning. Go to App and create a <Home />
 Home will have a view list of characters and above that will have a sort function
 
-Alright lets do some fetching, find out if this JOJO api is going to cut it.
+Alright lets do some fetching, find out if this JOJO api is going to cut it. cuts it, comes back as a single object, lets set it to state and get to dissecting it as needed, I think we will filter by series.
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
+import { fetchCharacters } from './services/fetchCharacters';
 import Home from './views/Home';
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let { StardustCrusaders, DiamondIsUnbreakable, GoldenWind } =
+          await fetchCharacters();
+        // console.log(data);
+        setCharacters([
+          ...StardustCrusaders,
+          ...DiamondIsUnbreakable,
+          ...GoldenWind,
+        ]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(characters);
+
   return (
     <>
       <h1>Hello World</h1>;
